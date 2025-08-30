7816e46c0f9c2bc513701fb97f4df1f6c7207aeb
BrunoSimonato <brunoosimonato@proton.me>
2025-08-29 22:55:59 -0400

    v2.0

--- restaura_hora.c
+++ restaura_hora.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include <locale.h>
+#include <unistd.h>
 #define ARQ_DATE "date.sh"
 
 int main(){
@@ -29,11 +30,16 @@ int main(){
 	printf("*****************************\n");
 	printf("Exemplo: \nDATA: 01/01/2025 \nHORA: 20:30:00\n");
 
-	printf("OBSERVAÇÃO \nNecessário executar como root!\n");
+	//printf("OBSERVAÇÃO \nNecessário executar como root!\n");
 	printf("Digite a data com 6 digitos: ");
 	scanf(" %d %c %d %c %d", &d1, &barra, &d2, &barra, &d3);
 	printf("Digite a hora com 6 digitos: ");
 	scanf(" %d %c %d  %c %d", &h1, &doispontos, &h2, &doispontos, &h3);
+	
+	if(getuid() != 0){
+	printf("Necessário  estar como usuário root\n");
+	return (1);	
+	}
 
 	fprintf(file, "#Data\n");
 	fprintf(file, "date -s %d/%d/%d\n", d3, d2, d1);
